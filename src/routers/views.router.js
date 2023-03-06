3a0b3c734c5f3a6f09356a119bc5b89c22ca5765
Casto Gil <casto782@gmail.com>
2023-03-06 17:50:01 -0300

    proyecto

---
+++ src/routers/views.router.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { allProducts } from "../Dao/controllerDb/productsManager.js";
+import {
+  realTimeAddProduct,
+  realTimeDeleteProduct,
+  realTimeProduct,
+  viewProduct,
+} from "../Dao/controllerDb/viewsManager.js";
+const router = express.Router();
+
+//Ruta principal vista de los productos
+router.get("/", viewProduct);
+//Ruta websocket
+router.get("/realtimeproducts", realTimeProduct);
+router.post("/realtimeproducts", realTimeAddProduct);
+router.delete("/realtimeproducts/:pid", realTimeDeleteProduct);
+
+export default router;
